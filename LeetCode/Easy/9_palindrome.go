f6b641201fffb8742f38cc58d7eb352a01a075db
Ethan Tang <tangyanhan@gmail.com>
2017-10-10 17:24:48 +0800

    palindrome for leetcode

---
+++ LeetCode/Easy/9_palindrome.go
@@ -0,0 +1,10 @@
+package Easy
+
+func isPalindrome(x int) bool {
+	tx, y := x, 0
+	for tx > 0 {
+		y = y * 10 + (tx % 10)
+		tx = tx / 10
+	}
+	return y == x
+}
---
+++ LeetCode/Easy/9_palindrome_test.go
@@ -0,0 +1,32 @@
+package Easy
+
+import "testing"
+
+func Test_IsPalindrome(t *testing.T) {
+	tests := []struct {
+		x int
+		expect bool
+	}{
+		{0, true},
+		{1, true},
+		{9, true},
+		{10, false},
+		{11, true},
+		{99, true},
+		{100, false},
+		{101, true},
+		{19422491, true},
+		{1000021, false},
+		{-19422491, false},
+		{-1, false},
+		{-9, false},
+		{-2147447412, false},
+	}
+
+	for i, test := range tests {
+		got := isPalindrome(test.x)
+		if got != test.expect {
+			t.Errorf("#%d: x=%d expect:%t got:%t", i, test.x, test.expect, got)
+		}
+	}
+}
